418d8a7ecffd84bcde17455d840368a4a2003094
Dana Ng <ngdana1@gmail.com>
2017-01-27 16:17:33 -0800

    added travis file and commented out testing

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL
+                       ||
+                      'mongodb://localhost/multipurpose-node';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+//                       'mongodb://localhost/blog-app-mongoose-challenge-solution';
+exports.PORT = process.env.PORT || 8080;
