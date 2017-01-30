c9ec3d5b76671fcd6631536d975e87f483c5a6d8
Dana Ng <ngdana1@gmail.com>
2017-01-30 11:00:00 -0800

    updated port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL
                        ||
-                      'mongodb://dummy:dummy123@ds131109.mlab.com:31109/riceball3';
-// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-//                       'mongodb://localhost/blog-app-mongoose-challenge-solution';
+                      'mongodb://localhost/multipurpose-node';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+                      'mongodb://localhost/multipurpose-node';
 exports.PORT = process.env.PORT || 8080;
