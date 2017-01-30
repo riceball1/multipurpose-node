559b77e6434a8b05eff1a1029b9622c9961922df
Dana Ng <ngdana1@gmail.com>
2017-01-30 12:02:36 -0800

    updated mocha/chai test by fixing database and server paths

--- test/test-multipurpose.js
+++ test/test-multipurpose.js
@@ -1,11 +1,26 @@
+const mongoose = require('mongoose');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const should = chai.should();
-const index = require('/routes/index');
 
+const {app, runServer, closeServer} = require('../server');
+const {DATABASE_URL, PORT} = require('../config');
+
+chai.use(chaiHttp);
 
 describe('index', function() {
+  // before the tests runs statr the server once
+  before(function() {
+    return runServer(DATABASE_URL);
+  });
+
+  // after all tests have been completed close out the server
+after(function() {
+  return closeServer();
+});
+
+
   it('should return homepage', function() {
     return chai.request(app)
       .get('/')
