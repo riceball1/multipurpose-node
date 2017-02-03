bd69986f7fc09ef02686dda998be3201ce0acbcc
Dana Ng <ngdana1@gmail.com>
2017-02-02 17:54:29 -0800

    added add item form - working

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,22 @@ router.get('/settings', ensureAdmin, (req, res) => {
   res.render('admin');
 });
 
+router.post('/newitem', (req, res) => {
+  const {itemName, imgSrc, shortDescription} = req.body;
+  let newItem = new Item({
+    itemName: itemName,
+    imgSrc: '/public/'+imgSrc,
+    shortDescription: shortDescription
+  });
+  newItem.save(function (err) {
+    if (err) {
+      return handleError(err);
+    }
+    req.flash('success_msg', "Successfully created new item!");
+    res.redirect('/admin/settings');
+  })
+});
+
 function ensureAdmin(req, res, next) {
   let admin = req.user.admin;
   if(req.isAuthenticated() && admin) {
