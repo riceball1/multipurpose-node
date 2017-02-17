71923ad07eb3d72984501104aeb60214e63ca351
Sebastian Bastidas <sbastidasr@gmail.com>
2017-02-17 09:19:24 -0800

    testing

--- test/test-multipurpose.js
+++ test/test-multipurpose.js
@@ -8,7 +8,7 @@ const should = chai.should();
 const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL, PORT } = require('../config/database');
 
-
+console.log(TEST_DATABASE_URL);
 const Tip = require('../models/tip');
 const User = require('../models/user');
 const Item = require('../models/item');
@@ -16,130 +16,138 @@ const Item = require('../models/item');
 chai.use(chaiHttp);
 
 function seedTipData() {
+
+
     console.info('seeding tip data');
     const seedData = [];
 
     for (let i = 1; i <= 5; i++) {
-        seedData.push(generateTipData());
+      seedData.push(generateTipData());
     }
+    //console.log(seedData);
     return Tip.insertMany(seedData);
+
+
 }
 
 // generate an object for seed data
 function generateTipData() {
-    return {
-        userId: faker.random.number,
-        upvote: 1,
-        downvote: 1,
-        content: faker.lorem.paragraph(),
-        itemId: faker.random.number,
-        userName: faker.random.firstName
-    }
+  return {
+    userId: mongoose.Types.ObjectId(faker.random.number()),
+    upvote: 1,
+    downvote: 1,
+    content: faker.lorem.paragraph(),
+    itemId: mongoose.Types.ObjectId(faker.random.number()),
+    userName: faker.name.firstName()
+  }
 }
 
 // delete the entire database
 function tearDownDb() {
-    console.warn('Deleting database');
-    return mongoose.connection.dropDatabase();
+  console.warn('Deleting database');
+  return mongoose.connection.dropDatabase();
 }
 
 describe('Multipurpose API', function() {
-    before(function() {
-        return runServer(TEST_DATABASE_URL);
-    });
-    beforeEach(function() {
-        return seedTipData();
-    });
-    afterEach(function() {
-        return tearDownDb();
-    });
-    after(function() {
-        return closeServer();
-    });
-});
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+  beforeEach(function() {
+    return seedTipData();
+  });
+  afterEach(function() {
+    return tearDownDb();
+  });
+  after(function() {
+    return closeServer();
+  });
 
+  it('should increment the upvote', function(done) {
+    // Tip
+    // .findOne()
+    // .exec()
+    // .then(function(tip) {
+    //
+    //   // let res;
+    //   // return chai.request(app)
+    //   // .get('/tips')
+    //   // .then((_res) => {
+    //   //   res = _res;
+    //   //   console.log(res);
+    //   //   done();
+    //   // })
+    //
+    //   chai.request(app)
+    //   .post(`/${tip.id}/upvote`)
+    //   .send(tip.id)
+    //   .then(function(res) {
+    //     res.should.have.status(201);
+    //     res.should.be.json;
+    //     res.body.should.be.a('object');
+    //     done()
+    //   })
+    //
+    // }
+    //})
 
-describe('POST endpoint', function() {
-    it.only('should increment the upvote', function(done) {
-        // Tip
-        //   .findOne()
-        //   .exec()
-        //   .then(function(tip) {
-          let res;
-        return chai.request(app)
-          .get('/tips')
-          .then((_res) => {
-            res = _res;
-            console.log(res);
-            done();
-          })
-        // chai.request(app)
-        //     .post(`/${tip.id}/upvote`)
-        //     .send(tip.id)
-        //     .then(function(res) {
-        //         res.should.have.status(201);
-        //         res.should.be.json;
-        //         res.body.should.be.a('object');
-        //     })
-            // })
-    });
+  });
 
-    it('should increment the downvote', function() {
-        Tip
-            .findOne()
-            .exec()
-            .then(function(tip) {
-                return chai.request(app)
-                    .post(`/${tip.id}/downvote`)
-                    .send(tip)
-                    .then(function(res) {
-                        res.should.have.status(201);
-                        res.should.be.json;
-                        res.body.should.be.a('object');
+  it('should increment the downvote', function() {
 
-                        resTip = res.body[0];
-                        return Tip.findById(resTip.id)
-                    })
-                    .then(function(tip) {
-                        tip.content.should.equal();
-                    })
-            })
-    });
+    Tip
+    .findOne()
+    .exec()
+    .then(function(tip) {
+      console.log("tip", tip);
+      return chai.request(app)
+      .post(`/${tip.id}/downvote`)
+      .send(tip)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
 
-});
-
-describe('PUT endpoint', function() {
-    it('should remove a tip ', function() {
-
-        Tip
-            .findOne()
-            .exec()
-            .then(function(tip) {
-                return chai.request(app)
-                    .put(`/${tip.id}/${tip.userId}/${tip.itemId}/deletetip`)
-                    .send(tip)
-                    .then(function(res) {
-                        res.should.have.status(201);
-                        res.should.be.json;
-                        res.body.should.be.a('object');
-                    })
-            })
-
-    });
-
-    it('should remove a bookmarked item', function() {
-        const user = new User({
-            userId: faker.random.number,
-            itemIdArray: [faker.random.number]
-        })
-        user.save((err, user) => {
-            return chai.request(app)
-                .put(`/${user.itemIdArray[0]}/deletebookmark`)
-                .send(user)
-                .then(function(res) {
-                    res.should.have.status(201);
-                    res.should.be.json;
-                })
-        });
+        resTip = res.body[0];
+        return Tip.findById(resTip.id)
+      })
+      .then(function(tip) {
+        tip.content.should.equal();
+      })
     })
+
+  });
+
+  it('should remove a tip ', function() {
+
+    Tip
+    .findOne()
+    .exec()
+    .then(function(tip) {
+      return chai.request(app)
+      .put(`/${tip.id}/${tip.userId}/${tip.itemId}/deletetip`)
+      .send(tip)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+      })
+    })
+
+  });
+
+  it('should remove a bookmarked item', function() {
+    const user = new User({
+      userId: faker.random.number,
+      itemIdArray: [faker.random.number]
+    })
+    user.save((err, user) => {
+      return chai.request(app)
+      .put(`/${user.itemIdArray[0]}/deletebookmark`)
+      .send(user)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+      })
+    });
+  })
 });
