1e5d7c832fdc10fa94f3d8b2af4b9469c506123e
Dana Ng <ngdana1@gmail.com>
2017-02-24 14:20:48 -0800

    updated with an env file to do local development

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
-const {DATABASE_LOCALTEST} = require('./database-local.js');
+
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL || DATABASE_LOCALTEST;
+                       global.DATABASE_URL;
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/multipurpose-test';
 
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.load();
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
